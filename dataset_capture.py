d54f47145caf236ff4f8cae4c80d0b5527b914cb
Abhinav Singh <abhinavsingh1907@gmail.com>
2020-09-19 06:49:20 +0530

    capturing and saving images

--- dataset_capture.py
+++ dataset_capture.py
@@ -20,4 +20,46 @@ face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 count = 0
 #calling function for checking dataset diretory
 assure_path_exists("dataset/")
+# Start looping
+while(True):
+
+    # start Capturing video frame
+    _, image_frame = vid_cam.read()
+
+    # Convert frame to grayscale for better accuracy
+    gray = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect frames of different sizes, list of faces rectangles
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+    # Loops for each faces
+    for (x,y,w,h) in faces:
+
+        # Crop the image frame into rectangle of face's size
+        cv2.rectangle(image_frame, (x,y), (x+w,y+h), (255,0,0), 2)
+        
+        # Increment sample face image
+        count += 1
+
+        # Save the captured image into the datasets folder with unique id
+        cv2.imwrite("dataset/Divyansh Singhal." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+
+        # Display the video frame, with bounded rectangle on the person's face
+        cv2.imshow('frame', image_frame)
+
+    # To stop taking video, press 'q' for at least 100ms or press ctrl+c
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        #will end the loop
+        break
+
+    # If image taken reach 30, stop taking video
+    elif count>=30:
+        print("Successfully Captured")
+        break
+
+# Stop video
+vid_cam.release()
+
+# Close all started windows
+cv2.destroyAllWindows()
 
